7c8b1abfaee021f7790c7a97ad65048d64616aa4
Jose-S <jose.sarvia99@gmail.com>
2020-02-23 18:58:54 -0500

    Added sidebar with data using Router components

---
+++ pharmd-app/src/students/StudenListMain.js
@@ -0,0 +1,54 @@
+import React, { useCallback, Fragment } from 'react';
+import { Route, useHistory } from 'react-router-dom';
+import StudentList from './StudentList';
+import { List, SimpleForm } from 'react-admin';
+import Drawer from '@material-ui/core/Drawer';
+import StudentEdit from './StudentEdit';
+
+const StudentListMain = props => {
+	const history = useHistory();
+
+	const handleClose = useCallback(() => {
+		history.push('/students');
+	}, [history]);
+
+	return (
+		<Route path='/students/:id'>
+			{({ match }) => {
+				// Double negative
+				const isMatch = !!(
+					match &&
+					match.params &&
+					match.params.id !== 'create'
+				);
+
+				return (
+					<Fragment>
+						<List {...props}>
+							<StudentList
+								selectedRow={isMatch && parseInt(match.params.id, 10)}
+							/>
+						</List>
+						<Drawer
+							varient='persistent'
+							open={isMatch}
+							anchor='right'
+							onClose={handleClose}
+						>
+							{/* Avoid route errors*/}
+							{isMatch ? (
+								<StudentEdit
+									id={match.params.id}
+									onCancel={handleClose}
+									{...props}
+								/>
+							) : null}
+						</Drawer>
+					</Fragment>
+				);
+			}}
+		</Route>
+	);
+};
+
+export default StudentListMain;
