8d2e7fa3e6fe36178c85daf03e44b6eea52d86d9
Christopher Myers <chessmyers@aol.com>
2020-02-27 19:57:09 -0500

    Add new folder structure, READMEs

--- pharmd-app/src/services/FileUploadService.js
+++ pharmd-app/src/services/FileUploadService.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
+import * as XLSX from 'xlsx';
 
 class FileUploadService {
+
+    static parseSpreadsheet (data) {
+
+    }
 
     static UploadSpreadsheet (data, config) {
         return axios.post("http://localhost:8000/upload", data, config);
