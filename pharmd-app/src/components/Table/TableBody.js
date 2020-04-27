5abe6d9e6fa8cc338e2ea22d33c6924dec743c4d
Jose Saravia <jose.sarvia99@gmail.com>
2020-04-27 18:31:35 -0500

    More File Cleaning

--- pharmd-app/src/components/Table/TableBody.js
+++ pharmd-app/src/components/Table/TableBody.js
@@ -1,3 +1,11 @@
+/**
+ * Description:
+ * TODO:
+ * Date: 04-26-2020
+ */
+
+//-------------------------- IMPORTS --------------------------
+
 import React from "react";
 
 import { DatagridBody as RaDatagridBody } from "react-admin";
@@ -8,7 +16,7 @@ import tw from "tailwind.macro";
 
 const DatagridBody = styled(RaDatagridBody)``;
 
-const TableBody = (props) => {
+const TableBody = props => {
   return <DatagridBody {...props} row={<TableRow />} />;
 };
 
