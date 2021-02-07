6f09c38401b72e25dd586808c22c074ae8d7594d
Christopher Myers <chessmyers@aol.com>
2021-02-07 13:26:05 -0800

    Add include student ID in request body

--- pharmd-app/src/components/Layout/NoteCreate.js
+++ pharmd-app/src/components/Layout/NoteCreate.js
@@ -3,7 +3,7 @@ import { Create, TextInput, SimpleForm, required } from 'react-admin';
 
 const NoteCreate = props => (
     <Create {...props} resource="notes" onSuccess={props.onSuccess}>
-        <SimpleForm>
+        <SimpleForm initialValues={ {studentId: props.id} }>
             <TextInput label="Note Title" source="title" validate={required()} />
             <TextInput label="Note Content" source="body" validate={required()} />
         </SimpleForm>
