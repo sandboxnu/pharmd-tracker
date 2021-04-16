06b175efc84c414ad0b02082132d5fbfc350ab96
kevinsebcam <kevinsebcam@gmail.com>
2021-04-15 23:35:11 -0400

    Having trouble with Tailwind animations, so using css file for now. Start adding css animations. Animation does not occur if the component always has an animation styling.

--- pharmd-app/src/components/Basic/BarGraph.js
+++ pharmd-app/src/components/Basic/BarGraph.js
@@ -6,28 +6,40 @@ import React, { useState } from "react";
 // Style Imports
 import { blue } from "@material-ui/core/colors";
 import tw, { styled, css } from "twin.macro";
+import "./Temp.css";
 
 const Bar = ({ max, min, height, threshold }) => {
   const heightStr = `${height.toString()}px`;
-
+  const [state, setState] = useState(0);
   // this bar covers (from threshold[0] to threshold[1])
   let secondHeight;
+  // const top =
+  let animate;
+  // const animate = "bar";
 
-  if (min > threshold[1] || max < threshold[0]) {
-    secondHeight = 0;
-  } else {
+  if (min >= threshold[1] || max <= threshold[0]) {
+    console.log(`bar `);
     secondHeight = height;
+    animate = "bar";
+    // setState(state + 1);
+  } else {
+    console.log("car");
+    secondHeight = 0;
+    animate = "car";
+    // setState(state + 1);
   }
 
   const secondHeightStr = `${secondHeight.toString()}px`;
   return (
     <div tw="flex-1" css={{ height: heightStr, "margin-right": ".1rem" }}>
-      <div tw="bg-primary h-full opacity-25 rounded-t-lg" />
+      <div tw="bg-primary h-full opacity-75 rounded-t-lg" />
       <div
-        // tw="bg-white relative rounded-t-lg w-full"
-        tw="animate-grow bg-primary opacity-75 relative rounded-t-lg w-full"
-        css={{ top: `-${secondHeightStr}` }}
+        tw="bg-white relative rounded-t-lg w-full"
+        // tw="bg-primary opacity-25 relative rounded-t-lg w-full"
+        css={{ height: secondHeightStr, top: `-${secondHeightStr}` }}
+        className={animate}
       >
+        <div tw="bg-primary h-full opacity-25 rounded-t-lg w-full" />
       </div>
     </div>
   );
