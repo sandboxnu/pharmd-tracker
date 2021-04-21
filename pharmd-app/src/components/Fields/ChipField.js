40b1c5aed1e35be412aebb0a50b015d572545cfa
Vera Kong <vkong6019@gmail.com>
2021-04-21 17:31:13 -0400

    refactor to lowercase and capital status (#87)
    * refactor to lowercase and capital status
    
    * refactor pill and font sizing

--- pharmd-app/src/components/Fields/ChipField.js
+++ pharmd-app/src/components/Fields/ChipField.js
@@ -1,22 +1,27 @@
 import React from "react";
 import tw, { styled } from "twin.macro";
-import { ChipField as RAChipField } from "react-admin";
+import { Chip as MuiChip } from "@material-ui/core";
 import { getFieldBackgroundColor, getFieldColor } from "../../themes/field-colors";
 
-const Pill = styled(RAChipField)`
-	
-   ${tw`rounded-lg capitalize w-28 fontStyle-4 font-bold tracking-wider`}
-   color: ${props => props.color};
-   background-color: ${props => props.backgroundColor};
-	
+const Pill = styled(MuiChip)`
+  ${tw`rounded-lg capitalize w-28 fontStyle-4 font-bold tracking-wider`}
+  font-size: 1.1em;
+  color: ${props => props.color};
+  background-color: ${props => props.backgroundColor};
 `;
 
 const ChipField = props => {
+  const readableStatus = props.record[props.source]
+    .replace("_", " ")
+    .split(" ")
+    .map(s => s.charAt(0) + s.slice(1).toLowerCase())
+    .join(" ");
+
   return (
     <Pill
+      label={readableStatus}
       color={getFieldColor(props)}
       backgroundColor={getFieldBackgroundColor(props)}
-      {...props}
     />
   );
 };
