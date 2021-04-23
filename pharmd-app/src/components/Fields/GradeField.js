d9a7d64a12becc8fd774dd64ebb83d0d6d7ecd46
nicolesorial <sorial.n@husky.neu.edu>
2021-04-22 20:35:36 -0400

    Display GPAs and percentages as doubles (#89)
    * grades as doubles
    
    * also in the quickinfofield
    
    Co-authored-by: nicolesorial <grace.cola@gmail.com>

--- pharmd-app/src/components/Fields/GradeField.js
+++ pharmd-app/src/components/Fields/GradeField.js
@@ -1,9 +1,10 @@
 import React from "react";
 import tw, { styled } from "twin.macro";
-import { NumberField } from "react-admin";
+import { FunctionField } from "react-admin";
 import TextField from "./TextField";
 import { getFieldColor } from "../../themes/field-colors";
 import { STUDENT_COURSE } from "../../constants/apiObjects";
+import { formatDecimal } from '../../services/Utility'
 
 const StyledTextField = styled(TextField)`
   ${tw`fontStyle-6 text-gray-700 font-medium`}
@@ -12,7 +13,7 @@ const StyledTextField = styled(TextField)`
   font-size: 1.4em;
 `;
 
-const StyledNumberField = styled(NumberField)`
+const StyledFunctionField = styled(FunctionField)`
   ${tw`fontStyle-6 text-gray-700 font-medium`}
   
   color: ${props => props.color};
@@ -20,15 +21,16 @@ const StyledNumberField = styled(NumberField)`
 `;
 
 const GradeField = props => {
-  return props.source === STUDENT_COURSE.LETTER_GRADE ? (
-    <StyledTextField color={getFieldColor(props)} {...props} />
-  ) : (
-    <StyledNumberField
-      color={getFieldColor(props)}
-      {...props}
-      options={{ maximumFractionDigits: 2 }}
-    />
-  );
+  return props.source === STUDENT_COURSE.LETTER_GRADE ?
+    (
+      <StyledTextField color={getFieldColor(props)} {...props} />
+    ) : (
+      <StyledFunctionField
+        render={record => `${formatDecimal(record[props.source])}`}
+        color={getFieldColor(props)}
+        {...props}
+      />
+    );
 };
 
 export default GradeField;
